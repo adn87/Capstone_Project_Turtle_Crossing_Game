2f3680d4940acd3ae409e0f55fd21d4d59c8e89a
adn87 <aaghadieh@gmail.com>
2023-10-30 04:49:21 -0400

    Turtle_Crossing_Game_002
    - finished up the car_manager class to create and generate random number of cars, that will move across the screen from left to right. Increase the speed of the cars every time the user levels up.
    - finished up the player class to interact with the "Up" key so the frog would move every time the key is pressed. If the frog gets to the finish line it goes back to the starting position.
    - finished up the scoreboard class to keep track of the player's level and if the frog collides with a car the screen will print out a "GAME OVER" message.
    - finished up the main class, created if statements to detect if the frog gets to the top of the screen, and to detect if frog collides with a car.

--- main.py
+++ main.py
@@ -8,7 +8,30 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+player = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(fun=player.step, key="Up")
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    car_manager.create_car()
+    car_manager.car_movement()
+
+    # Detect if turtle gets to the top of the screen
+    if player.is_at_finish_line():
+        player.go_to_start()
+        car_manager.level_up()
+        scoreboard.points()
+
+    # Detect collision with car_manager.
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+
+screen.exitonclick()
